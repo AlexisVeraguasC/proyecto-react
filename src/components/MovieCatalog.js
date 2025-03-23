cfe04e933e7c521e23145bbe3ac26fd01eb8f3b0
AlexisVeraguasC <al.veraguas@duocuc.cl>
2025-03-23 17:03:57 -0300

    Agregando imágenes de cada movie

--- src/components/MovieCatalog.js
+++ src/components/MovieCatalog.js
@@ -6,6 +6,13 @@ const MovieCatalog = () => {
   const [movies, setMovies] = useState([]);
   const [searchQuery, setSearchQuery] = useState("");
 
+// La base URL para las imágenes en TMDb
+    const imageBaseUrl = "https://image.tmdb.org/t/p/w500";
+
+
+// Imagen por defecto si no hay póster
+  const defaultImage = "https://via.placeholder.com/500x750?text=No+Image";
+
   // Función que carga las películas de la API
   useEffect(() => {
     fetch("https://api.themoviedb.org/3/movie/popular?api_key=ee47c662e9f86c6eae463f09d0c72bb5")
@@ -31,6 +38,11 @@ const MovieCatalog = () => {
         {filteredMovies.map((movie) => (
           <div className="movie-card" key={movie.id}>
             <h2>{movie.title}</h2>
+            <img
+              src={movie.poster_path ? `${imageBaseUrl}${movie.poster_path}` : defaultImage}
+              alt={movie.title}
+              style={{ width: "200px", borderRadius: "8px" }}
+            />
             <p>{movie.overview}</p>
             <p><strong>Fecha de lanzamiento:</strong> {movie.release_date}</p>
           </div>
